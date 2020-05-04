785720fe51e49cde9548fd84e8c20e5e240b7bdd
PJL1128 <2238426765@qq.com>
2020-05-04 21:32:38 +0800

    今日提交

---
+++ js/enterprise.js
@@ -0,0 +1,15 @@
+$('.part-one-bottom h2').on('click', function () {
+    $(this).next().slideToggle(500).prev().children('i').toggleClass('active')
+})
+
+$('.part-three-bottom h2').on('click', function () {
+    $(this).next().slideToggle().prev().children('i').toggleClass('active')
+})
+
+$('.part-two-bottom h2').on('click', function () {
+    $(this).next().slideToggle().prev().children('i').toggleClass('active')
+})
+
+$('.part-four-bottom h2').on('click', function () {
+    $(this).next().slideToggle().prev().children('i').toggleClass('active')
+})
